c740b35b4fbff865c2965f14c4e9ed6c704b58d6
Amit adsum <amit.adsum@gmail.com>
2021-04-19 09:04:52 +0530

    Initial Commit

---
+++ sidemenu_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sidemenu_SwiftUI
+//
+//  Created by Adsum MAC 2 on 19/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sidemenu_SwiftUI/sidemenu_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidemenu_SwiftUIApp.swift
+//  sidemenu_SwiftUI
+//
+//  Created by Adsum MAC 2 on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct sidemenu_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
